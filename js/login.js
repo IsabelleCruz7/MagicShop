6325a3fe4ea5b7ff2478db71e019ec158d54806e
Laís Cristina <lais.melo@fitbank.com.br>
2021-07-02 15:11:57 -0300

    Tela de login validada

--- js/login.js
+++ js/login.js
@@ -1,7 +1,30 @@
+
 function validarLogin(){
-    var un = document.getElementById("un");
-    console.log("ok");
-    if(us == ""){
-        //alert("ERRO");
+
+    var un = document.getElementById("un").value;
+    var pass = document.getElementById("pass").value;
+
+    var usernameOk = un.length >= 6 && un.length <= 20? true : false;
+    var passOk = pass.length >= 4 && pass.length <= 10? true : false;
+    
+    if(passOk && usernameOk){
+        
+        
+           window.location.href = 'TelaUsuarioLogado.html';
+       
     }
+
+    if(!usernameOk){
+        alert('Nome de usuario inválido!');
+    }
+ 
+    if(!passOk){
+        alert('Senha inválida');
+    }
+
+   
+}
+
+function voltarHome(){
+    window.location.href = 'index.html';
 }
